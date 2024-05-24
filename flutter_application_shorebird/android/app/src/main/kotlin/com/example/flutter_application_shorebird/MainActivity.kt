c5cd05a8d9470ff130bdc5206ecd9ce12281b66f
ducafecat <ducafecat@gmail.com>
2024-05-24 11:15:23 +0800

    add -> Shorebird

---
+++ flutter_application_shorebird/android/app/src/main/kotlin/com/example/flutter_application_shorebird/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.flutter_application_shorebird
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
---
+++ flutter_application_shorebird/lib/main.dart
@@ -0,0 +1,247 @@
+import 'package:flutter/material.dart';
+
+import 'package:restart_app/restart_app.dart';
+import 'package:package_info_plus/package_info_plus.dart';
+import 'package:shorebird_code_push/shorebird_code_push.dart';
+
+final _shorebirdCodePush = ShorebirdCodePush();
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.blue),
+        useMaterial3: true,
+      ),
+      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({super.key, required this.title});
+
+  final String title;
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  int _counter = 0;
+  String? _version;
+
+  final _isShorebirdAvailable = _shorebirdCodePush.isShorebirdAvailable();
+  int? _currentPatchVersion;
+  bool _isCheckingForUpdate = false;
+
+  void _init() async {
+    PackageInfo packageInfo = await PackageInfo.fromPlatform();
+    if (mounted) {
+      setState(() {
+        _version = packageInfo.version;
+      });
+    }
+
+    _shorebirdCodePush.currentPatchNumber().then((currentPatchVersion) {
+      if (!mounted) return;
+      setState(() {
+        _currentPatchVersion = currentPatchVersion;
+      });
+    });
+  }
+
+  @override
+  void initState() {
+    super.initState();
+
+    // Get the current patch number and print it to the console. It will be
+    // null if no patches are installed.
+    // shorebirdCodePush
+    //     .currentPatchNumber()
+    //     .then((value) => print('current patch number is $value'));
+
+    _init();
+  }
+
+  Future<void> _checkForUpdate() async {
+    setState(() {
+      _isCheckingForUpdate = true;
+    });
+
+    // Ask the Shorebird servers if there is a new patch available.
+    final isUpdateAvailable =
+        await _shorebirdCodePush.isNewPatchAvailableForDownload();
+
+    if (!mounted) return;
+
+    setState(() {
+      _isCheckingForUpdate = false;
+    });
+
+    if (isUpdateAvailable) {
+      _showUpdateAvailableBanner();
+    } else {
+      ScaffoldMessenger.of(context).showSnackBar(
+        const SnackBar(
+          content: Text('No update available'),
+        ),
+      );
+    }
+  }
+
+  void _showDownloadingBanner() {
+    ScaffoldMessenger.of(context).showMaterialBanner(
+      const MaterialBanner(
+        content: Text('Downloading...'),
+        actions: [
+          SizedBox(
+            height: 14,
+            width: 14,
+            child: CircularProgressIndicator(
+              strokeWidth: 2,
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+
+  void _showUpdateAvailableBanner() {
+    ScaffoldMessenger.of(context).showMaterialBanner(
+      MaterialBanner(
+        content: const Text('Update available'),
+        actions: [
+          TextButton(
+            onPressed: () async {
+              ScaffoldMessenger.of(context).hideCurrentMaterialBanner();
+              await _downloadUpdate();
+
+              if (!mounted) return;
+              ScaffoldMessenger.of(context).hideCurrentMaterialBanner();
+            },
+            child: const Text('Download'),
+          ),
+        ],
+      ),
+    );
+  }
+
+  void _showRestartBanner() {
+    ScaffoldMessenger.of(context).showMaterialBanner(
+      const MaterialBanner(
+        content: Text('A new patch is ready!'),
+        actions: [
+          TextButton(
+            // Restart the app for the new patch to take effect.
+            onPressed: Restart.restartApp,
+            child: Text('Restart app'),
+          ),
+        ],
+      ),
+    );
+  }
+
+  Future<void> _downloadUpdate() async {
+    _showDownloadingBanner();
+
+    await Future.wait([
+      _shorebirdCodePush.downloadUpdateIfAvailable(),
+      // Add an artificial delay so the banner has enough time to animate in.
+      Future<void>.delayed(const Duration(milliseconds: 250)),
+    ]);
+
+    if (!mounted) return;
+
+    ScaffoldMessenger.of(context).hideCurrentMaterialBanner();
+    _showRestartBanner();
+  }
+
+  void _incrementCounter() {
+    setState(() {
+      _counter++;
+
+      // _checkForUpdates();
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    final theme = Theme.of(context);
+    final heading = _currentPatchVersion != null
+        ? '$_currentPatchVersion'
+        : 'No patch installed';
+
+    return Scaffold(
+      appBar: AppBar(
+        // TRY THIS: Try changing the color here to a specific color (to
+        // Colors.amber, perhaps?) and trigger a hot reload to see the AppBar
+        // change color while the other colors stay the same.
+        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
+        // Here we take the value from the MyHomePage object that was created by
+        // the App.build method, and use it to set our appbar title.
+        title: Text(widget.title),
+      ),
+      body: Center(
+        // Center is a layout widget. It takes a single child and positions it
+        // in the middle of the parent.
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            const Text('Current version:'),
+            Text(
+              _version ?? '-',
+              style: theme.textTheme.headlineMedium,
+            ),
+            const Text('Current patch:'),
+            Text(
+              heading,
+              style: theme.textTheme.headlineMedium,
+            ),
+            const SizedBox(height: 20),
+            if (!_isShorebirdAvailable)
+              Text(
+                'Shorebird Engine not available.',
+                style: theme.textTheme.bodyLarge?.copyWith(
+                  color: theme.colorScheme.error,
+                ),
+              ),
+            if (_isShorebirdAvailable)
+              ElevatedButton(
+                onPressed: _isCheckingForUpdate ? null : _checkForUpdate,
+                child: _isCheckingForUpdate
+                    ? const _LoadingIndicator()
+                    : const Text('Check for update'),
+              ),
+          ],
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: _incrementCounter,
+        tooltip: 'Increment',
+        child: const Icon(Icons.add),
+      ), // This trailing comma makes auto-formatting nicer for build methods.
+    );
+  }
+}
+
+class _LoadingIndicator extends StatelessWidget {
+  const _LoadingIndicator();
+
+  @override
+  Widget build(BuildContext context) {
+    return const SizedBox(
+      height: 14,
+      width: 14,
+      child: CircularProgressIndicator(strokeWidth: 2),
+    );
+  }
+}
